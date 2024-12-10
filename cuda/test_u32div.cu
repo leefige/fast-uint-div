c0551dec537ac2ba3b15940ba4cb62c4b59e2857
leefige <liyifei.411@outlook.com>
2024-12-11 01:10:00 +0800

    cuda: refactor test.

--- cuda/test_u32div.cu
+++ cuda/test_u32div.cu
@@ -7,6 +7,7 @@
 #include <memory>
 #include <random>
 #include <thread>
+#include <utility>
 #include <vector>
 
 #define CHECK_CUDA(expr)                                                       \
@@ -29,41 +30,54 @@
     }                                                                          \
   } while (0)
 
-static constexpr int WARP_SIZE = 32;
-static constexpr int TEST_COUNT = 1 << 23;
-static constexpr int THREAD_COUNT = 16;
+static constexpr int HOST_THREAD_COUNT = 16;
+static constexpr int CTA_SIZE = 256;
+static constexpr int TEST_COUNT = 1 << 24;
 
 template <int N, typename T> struct alignas(sizeof(uint4)) Vec {
   T data[N];
 };
 
 // one thread computes all elements
-__global__ void reference(uint32_t *out, const uint32_t *dividends,
-                          const U32Div div) {
+template <int BLOCK>
+__global__ void __launch_bounds__(BLOCK, 1)
+    kernel(uint32_t *out, const uint32_t *dividends, const U32Div div) {
   const uint32_t d = div.GetD();
 
   constexpr int UNROLL = 16 / sizeof(uint32_t);
-  static_assert(TEST_COUNT % UNROLL == 0, "requires TEST_COUNT % UNROLL == 0");
   using VecT = Vec<UNROLL, uint32_t>;
   static_assert(sizeof(VecT) == sizeof(uint4),
                 "assert sizeof(VecT) == sizeof(uint4)");
+  static_assert(TEST_COUNT % (BLOCK * UNROLL) == 0,
+                "requires TEST_COUNT % (BLOCK * UNROLL) == 0");
 
-  constexpr int N_LOOP = TEST_COUNT / UNROLL;
-  for (int i = 0; i < N_LOOP; ++i) {
-    VecT v_in;
-    const VecT *in_ptr = reinterpret_cast<const VecT *>(dividends) + i;
-    v_in = *in_ptr;
+  VecT v_in;
+  const VecT *in_ptr = reinterpret_cast<const VecT *>(dividends) +
+                       blockIdx.x * BLOCK + threadIdx.x;
+  v_in = *in_ptr;
 
-    VecT v_out;
+  VecT v_out;
 #pragma unroll
-    for (int k = 0; k < UNROLL; ++k) {
-      v_out.data[k] = v_in.data[k] / d;
-    }
+  for (int k = 0; k < UNROLL; ++k) {
+    v_out.data[k] = v_in.data[k] / d;
+  }
 
-    VecT *out_ptr = reinterpret_cast<VecT *>(out) + i;
-    if (blockIdx.x == 0 && threadIdx.x == 0) {
-      *out_ptr = v_out;
-    }
+  VecT *out_ptr =
+      reinterpret_cast<VecT *>(out) + blockIdx.x * BLOCK + threadIdx.x;
+  *out_ptr = v_out;
+  return;
+}
+
+template <typename Func>
+void host_threading(std::vector<std::thread> &thds, Func &&func) {
+  if (thds.size() < HOST_THREAD_COUNT) {
+    thds.resize(HOST_THREAD_COUNT);
+  }
+  for (int i = 0; i < HOST_THREAD_COUNT; ++i) {
+    thds[i] = std::thread(std::forward<Func>(func), i);
+  }
+  for (int i = 0; i < HOST_THREAD_COUNT; ++i) {
+    thds[i].join();
   }
   return;
 }
@@ -75,44 +89,34 @@ void test_body(const U32Div &div, bool large_n = false) {
   float total_time_fast = 1;
 
   std::vector<uint32_t> n_h(TEST_COUNT);
-  std::vector<std::thread> thds(THREAD_COUNT);
+  std::vector<std::thread> thds(HOST_THREAD_COUNT);
 
   /* Step 1: generate dividends */
-  static_assert(TEST_COUNT % THREAD_COUNT == 0,
-                "requires TEST_COUNT % THREAD_COUNT == 0");
-  constexpr int ELEM_PER_THREAD = TEST_COUNT / THREAD_COUNT;
-  for (int i = 0; i < THREAD_COUNT; ++i) {
-    thds[i] = std::thread([&, i]() {
-      using Dist = std::uniform_int_distribution<uint32_t>;
-      std::default_random_engine rng(i);
-      std::shared_ptr<Dist> dist;
-      if (large_n) {
-        dist = std::make_shared<Dist>(0, UINT32_MAX);
-      } else {
-        dist = std::make_shared<Dist>(0, INT32_MAX);
-      }
+  static_assert(TEST_COUNT % HOST_THREAD_COUNT == 0,
+                "requires TEST_COUNT % HOST_THREAD_COUNT == 0");
+  constexpr int ELEM_PER_THREAD = TEST_COUNT / HOST_THREAD_COUNT;
+  host_threading(thds, [&](int i) {
+    using Dist = std::uniform_int_distribution<uint32_t>;
+    std::default_random_engine rng(i);
+    std::shared_ptr<Dist> dist;
+    if (large_n) {
+      dist = std::make_shared<Dist>(0, UINT32_MAX);
+    } else {
+      dist = std::make_shared<Dist>(0, INT32_MAX);
+    }
 
-      for (int j = 0; j < ELEM_PER_THREAD; ++j) {
-        n_h[i * ELEM_PER_THREAD + j] = (*dist)(rng);
-      }
-    });
-  }
-  for (int i = 0; i < THREAD_COUNT; ++i) {
-    thds[i].join();
-  }
+    for (int j = 0; j < ELEM_PER_THREAD; ++j) {
+      n_h[i * ELEM_PER_THREAD + j] = (*dist)(rng);
+    }
+  });
 
   /* Step 2: run host */
   std::vector<uint32_t> out_h(TEST_COUNT);
-  for (int i = 0; i < THREAD_COUNT; ++i) {
-    thds[i] = std::thread([&, i]() {
-      for (int j = 0; j < ELEM_PER_THREAD; ++j) {
-        out_h[i * ELEM_PER_THREAD + j] = n_h[i * ELEM_PER_THREAD + j] / d;
-      }
-    });
-  }
-  for (int i = 0; i < THREAD_COUNT; ++i) {
-    thds[i].join();
-  }
+  host_threading(thds, [&](int i) {
+    for (int j = 0; j < ELEM_PER_THREAD; ++j) {
+      out_h[i * ELEM_PER_THREAD + j] = n_h[i * ELEM_PER_THREAD + j] / d;
+    }
+  });
 
   /* Step 3: copy dividends to device */
   uint32_t *n_d;
@@ -127,12 +131,15 @@ void test_body(const U32Div &div, bool large_n = false) {
   cudaEvent_t ref_start, ref_stop;
   CHECK_CUDA(cudaEventCreate(&ref_start));
   CHECK_CUDA(cudaEventCreate(&ref_stop));
+
+  static constexpr int CTA_COUNT =
+      TEST_COUNT / (CTA_SIZE * 16 / sizeof(uint32_t));
   // warmup
-  reference<<<1, WARP_SIZE>>>(ref_d, n_d, div);
+  kernel<CTA_SIZE><<<CTA_COUNT, CTA_SIZE>>>(ref_d, n_d, div);
   CHECK_KERNEL();
   // run
   CHECK_CUDA(cudaEventRecord(ref_start));
-  reference<<<1, WARP_SIZE>>>(ref_d, n_d, div);
+  kernel<CTA_SIZE><<<CTA_COUNT, CTA_SIZE>>>(ref_d, n_d, div);
   CHECK_KERNEL();
   CHECK_CUDA(cudaEventRecord(ref_stop));
   CHECK_CUDA(cudaEventSynchronize(ref_stop));
@@ -143,28 +150,23 @@ void test_body(const U32Div &div, bool large_n = false) {
   CHECK_CUDA(cudaMemcpy(ref_h.data(), ref_d, ref_h.size() * sizeof(uint32_t),
                         cudaMemcpyDeviceToHost));
   CHECK_CUDA(cudaFree(ref_d));
-  for (int i = 0; i < THREAD_COUNT; ++i) {
-    thds[i] = std::thread([&, i]() {
-      for (int j = 0; j < ELEM_PER_THREAD; ++j) {
-        int idx = i * ELEM_PER_THREAD + j;
-        if (out_h[idx] != ref_h[idx]) {
-          printf("Error: %u / %u = %u, Ref returns: %u\n", n_h[idx], d,
-                 out_h[idx], ref_h[idx]);
-          break;
-        }
+  host_threading(thds, [&](int i) {
+    for (int j = 0; j < ELEM_PER_THREAD; ++j) {
+      int idx = i * ELEM_PER_THREAD + j;
+      if (out_h[idx] != ref_h[idx]) {
+        printf("Error: %u / %u = %u, Ref returns: %u\n", n_h[idx], d,
+               out_h[idx], ref_h[idx]);
+        break;
       }
-    });
-  }
-  for (int i = 0; i < THREAD_COUNT; ++i) {
-    thds[i].join();
-  }
+    }
+  });
 
   // free
   CHECK_CUDA(cudaFree(n_d));
 
   total_time_slow *= 1000;
   total_time_fast *= 1000;
-  printf("d: %u,\tslow: %f us,\tfast: %f us,\tspeedup: %f\n", d,
+  printf("d: %u,\tslow: %.2f us,\tfast: %.2f us,\tspeedup: %f\n", d,
          total_time_slow, total_time_fast, total_time_slow / total_time_fast);
   return;
 }
